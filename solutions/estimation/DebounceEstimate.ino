96bbd8da4aed0fbb1d984cefdd14c5e19d530727
gioele-bernardini <gioele.g@outlook.com>
2024-07-16 13:47:58 +0200

    debounce estimation started.

--- solutions/estimation/DebounceEstimate.ino
+++ solutions/estimation/DebounceEstimate.ino
@@ -1,3 +1,38 @@
 /*
-This programs automatically detects the debounce estimation for the button in my own personal circuit.
+
 */
+
+const int buttonPin = 2;
+const int ledPin = 13;
+
+int buttonState = 0;
+int lastButtonState = 0;
+int buttonPressCount = 0;
+
+// Required for the Toggle mode
+int ledState = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  // Check for state change and count button presses
+  if (buttonState != lastButtonState) {
+    if (buttonState == HIGH) {
+      buttonPressCount++;
+      Serial.println(buttonPressCount);
+    }
+
+    if (buttonState == HIGH) {
+      ledState = !ledState;
+      digitalWrite(ledPin, ledState);
+
+      lastButtonState = buttonState;
+    }
+}
+
